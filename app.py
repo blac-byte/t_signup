f81d3e62251b92a53e001fee0e3b6938f4384d93
Anandhu-Ajayakumar <anandhuajayakumar59@gmail.com>
2025-09-16 11:43:29 +0530

    added checks to routes, secure routes

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, url_for, redirect, session, flash
+from werkzeug.security import check_password_hash as hash_check, generate_password_hash as hash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import timedelta
 
@@ -13,9 +14,9 @@ db=SQLAlchemy(app)
 #__________________________________________________________________________________________
 
 class student(db.Model):
-    id=db.Column(db.Integer,primary_key=True)
-    stud_email=db.Column(db.String(100), primary_key=True)
-    stud_pass=db.Column(db.String(100))
+    id=db.Column(db.Integer,primary_key=True,autoincrement=True)
+    stud_email=db.Column(db.String(100),unique=True,nullable=False)
+    stud_pass=db.Column(db.String(100),nullable=False)
 
     def __init__(self,stud_email,stud_pass):
         self.stud_email=stud_email
@@ -30,19 +31,59 @@ with app.app_context():
 def index():
         return render_template('signup.html')
 
+# The code block below is the watchdog for the code block above
+# Basically checks all the actions of '/'
+
+@app.route('/signup_check',methods=['POST','GET'])          
+def signup_check():
+        if request.method=='POST':
+              email=request.form.get('email')
+              password=request.form.get('password')
+              hash_password=hash(password)
+              session['email']=email
+            #   if email and password in db:
+            #     return 'You already have an account'
+            #   elif  email and passord not in db:
+            #        create a new row in db
+
+              print(f'Email : {email}')
+              print(f'Password : {password}')
+              return redirect(url_for('dashboard'))
+            #   if email in db: 
+            #     db.session.add(student(email,password))
+            #     db.session.commit()
+            #     return redirect(url_for('dashboard'))
+        else:
+            return render_template('index.html')
+
 #__________________________________________________________________________________________
 
+@app.route('/signin',methods=['POST','GET'])          
+def signin():
+        return render_template('signin.html')
 
-@app.route('/page',methods=['GET','POST'])
-def page():
-       if request.method=='POST':
-            get_email=request.form.get('email')
-            get_pass=request.form.get('password')
-            db.session.add(student(get_email,get_pass))
-            db.session.commit()
-            return f'{get_email} {get_pass}'
-       else:
-            return render_template('page.html')
+# The code block below is the watchdog for the code block above
+# Basically checks all the actions of '/signin'
+
+
+@app.route('/signin_check',methods=['POST','GET'])          
+def signin_check():
+    if request.method=='POST':
+         if 'email' in session:
+              return redirect(url_for('dashboard'))
+         else:
+            email=request.form.get('email')
+            password=request.form.get('password')
+            # if hash_check(db_password,password):
+            session['email']=email
+            return redirect(url_for('dashboard'))
+    else:
+        return redirect(url_for('index'))
+#__________________________________________________________________________________________
+
+@app.route('/dashboard',methods=['GET','POST'])
+def dashboard():
+    return render_template('dashboard.html')
 #__________________________________________________________________________________________
 
 
